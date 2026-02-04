15f1d3ec5061821d55991af88320cb1e21ef7ce4
Jimy Sebastian Angarita Triana <jimy_sangarita@soy.sena.edu.co>
2026-02-04 08:43:35 -0500

    fead: Modificiacion de archivos para funcionalidad de gestion de permisos

--- MINEGATE_SENA/MINEGATE_SENA/urls.py
+++ MINEGATE_SENA/MINEGATE_SENA/urls.py
@@ -24,3 +24,4 @@ urlpatterns = [
     path('visita-interna/', include('visitaInterna.urls')),
     path('visita-externa/', include('visitaExterna.urls')),
 ]
+
--- MINEGATE_SENA/core/urls.py
+++ MINEGATE_SENA/core/urls.py
@@ -9,13 +9,4 @@ urlpatterns = [
     path(
         "panel_administrativo/", views.panel_administrativo, name="panel_administrativo"
     ),
-    path(
-        "gestionar_permisos/", views.gestionar_permisos, name="gestionar_permisos"
-    ),
-    path(
-        "aprobar_usuario/<int:usuario_id>/", views.aprobar_usuario, name="aprobar_usuario"
-    ),
-    path(
-        "rechazar_usuario/<int:usuario_id>/", views.rechazar_usuario, name="rechazar_usuario"
-    ),
 ]
