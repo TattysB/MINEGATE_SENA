3b1146ff135c18bc21f0e5f77ed7ccdad29dfaa0
Elyan Alejandro Cristancho Castañeda <elyancristancho123@gmail.com>
2026-02-04 10:18:11 -0500

    feat: se modificaron archivos correspondientes al funcionamiento de la aplicacion de visita externa

--- MINEGATE_SENA/MINEGATE_SENA/urls.py
+++ MINEGATE_SENA/MINEGATE_SENA/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
     path('auth/',include('usuarios.urls')),
-    path('visita-interna/', include('visitaInterna.urls')),
-    path('visita-externa/', include('visitaExterna.urls')),
+    path('visita_interna/', include('visitaInterna.urls')),
+    path('visita_externa/', include('visitaExterna.urls')),
 ]
