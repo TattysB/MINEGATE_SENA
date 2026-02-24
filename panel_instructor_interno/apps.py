c5cd32aa9400356b0133922eda9daa60ab363d07
Elyan Alejandro Cristancho Castañeda <elyancristancho123@gmail.com>
2026-02-24 10:47:22 -0500

    papu

--- panel_instructor_interno/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PanelInstructorInternoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'panel_instructor_interno'
-    verbose_name = 'Panel Instructor Interno'
--- panel_visitante/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PanelVisitanteConfig(AppConfig):
-    name = 'panel_visitante'
